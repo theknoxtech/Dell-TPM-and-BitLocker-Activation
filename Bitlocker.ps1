ae48f7ace90985583a6de36e950e6ee41a73a9ba
The Knox Tech <119535757+theknoxtech@users.noreply.github.com>
2024-04-15 14:04:37 -0400

    Added else

--- Bitlocker.ps1
+++ Bitlocker.ps1
@@ -4,10 +4,11 @@ function Get-SMBiosVersion {
     $Bios = Get-CimInstance Win32_BIOS 
     $Version = [float]::Parse("$($bios.SMBIOSMajorVersion).$($bios.SMBIOSMinorVersion)")
     
-
     if ($Version -ge "2.4"){
-
-        Write-Host "The bios version is"
+        Write-Host "Version is compatible"
+    }
+    else{
+        Write-Host "Version is not compatible" -ErrorAction Stop
     }
 
 }
