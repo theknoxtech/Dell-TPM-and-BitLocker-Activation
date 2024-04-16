ed2c8ea5c4c2a649de96f9f5e84eac8f25cedb38
The Knox Tech <119535757+theknoxtech@users.noreply.github.com>
2024-04-16 11:13:26 -0400

    Get TPM state

--- Bitlocker.ps1
+++ Bitlocker.ps1
@@ -1,30 +1,30 @@
-#Bios Check
+#Bios verion check
 function Get-SMBiosVersion {
     $Bios = Get-CimInstance Win32_BIOS 
     $Version = [float]::Parse("$($bios.SMBIOSMajorVersion).$($bios.SMBIOSMinorVersion)")
-    
-    if ($Version -ge "2.4"){
-        Write-Host "Version is compatible"
-    }
-    else{
-        Write-Host "Version is not compatible" -ErrorAction Stop
-    }
-
+    return $Version
 }
 
-Get-SMBiosVersion
+#Get-SMBiosVersion
 
-#TPM Check
+TPM Check
 
 function Get-TPMState {
 
-    $CheckTPM = Get-Tpm | Select-Object TpmPresent
-
-    if ($CheckTPM -eq $false){
-        Return "TPM is not present "
+    $TPMEnabled = Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -class 'win32_tpm' | Select-Object -Property IsEnabled_InitialValue
+    $TPMActive = Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -class 'win32_tpm' | Select-Object -Property IsActivated_InitialValue
+    if ($TPMEnabled){
+        Return $TPMEnabled
+        elseif ($TPMActive) {
+            Return "TPM Enabled: $($TPMEnabled)"+"TPM Active: $($TPMActive)"
+        }
     }
-    Return "TPM is present"
+    Return "TPM is not found"
+
+
 
 }
 
 Get-TPMState
+
+
