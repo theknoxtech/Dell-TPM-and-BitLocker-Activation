aebff17a951970c5ec0f355d94d7408893807e55
The Knox Tech <119535757+theknoxtech@users.noreply.github.com>
2024-04-17 09:26:01 -0400

    create if statements

--- Func_Get_TPMState.ps1
+++ Func_Get_TPMState.ps1
@@ -1,10 +1,20 @@
 function Get-TPMState {
 
-    $TPMCheck1 = Get-Tpm | Select-Object -Property 
-    return $TPM
+    $Check1 = Get-Tpm | Select-Object -Property TpmPresent
+    $Check2 = Get-CimInstance -Namespace 'root/cimv2/Security/MicrosoftTpm' -ClassName 'win32_tpm' | Select-Object IsEnabled_InitialValue
+    
+    if ($Check1){
+        Write-Host "TPM is present" 
+        if ($Check2) {
+            Return "TPM is enabled"
+            }
+
+        }
+    }
+
 
     #Return "TPM is not found"
-}
+
 
 Get-TPMState
 
